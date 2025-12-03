13b8ee16997f1992d66fdc9d3b036c07faf6f9e5
radu-mocanu <radu.mocanu@uipath.com>
2025-12-03 15:12:48 +0200

    chore: add error category

--- src/uipath/core/errors/errors.py
+++ src/uipath/core/errors/errors.py
@@ -1,9 +1,21 @@
 """UiPath common exceptions."""
 
+from enum import Enum
+
+
+class ErrorCategory(str, Enum):
+    """Categories of UiPath errors."""
+
+    DEPLOYMENT = "Deployment"
+    SYSTEM = "System"
+    UNKNOWN = "Unknown"
+    USER = "User"
+
 
 class UiPathFaultedTriggerError(Exception):
     """UiPath resume trigger error."""
 
+    category: ErrorCategory
     message: str
 
 
